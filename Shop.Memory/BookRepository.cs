f465ba0e8349708e12806bc4dc793ed4f1a80568
s.gnevushev <s.gnevushev@gmail.com>
2020-05-05 19:55:44 +0300

    completion BookRepository

--- Shop.Memory/BookRepository.cs
+++ Shop.Memory/BookRepository.cs
@@ -1,8 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Shop.Memory
 {
-    public class Class1
+    public class BookRepository : IBookRepository
     {
+
+        private readonly List<Book> books = new List<Book> { 
+            new Book(1, "Тихий Дон"),
+            new Book(2, "Мартин Иден"),
+            new Book(3, "Таинсвенный остров")
+        };
+
+ 
+        public List<Book> GetAllByTitle(string titlePart)
+        {
+            return books.Where(x => x.Title.Contains(titlePart)).ToList();
+        }
     }
 }
+ 
--- domain/Shop/Book.cs
+++ domain/Shop/Book.cs
@@ -13,7 +13,6 @@ namespace Shop
             Id = id;
             Title = title;
         }
-        
     }
 }
  
