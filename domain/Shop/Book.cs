b87743dedf8bdf2a3d689dd8b21045d0ee3f02c0
s.gnevushev <s.gnevushev@gmail.com>
2020-05-05 19:46:41 +0300

    add interface

--- domain/Shop/Book.cs
+++ domain/Shop/Book.cs
@@ -4,5 +4,16 @@ namespace Shop
 {
     public class Book
     {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public Book(Guid id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+        
     }
 }
+ 
---
+++ domain/Shop/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop
+{
+    public interface IBookRepository
+    {
+        List<Book> GetAllByTitle(string titlePart);
+    }
+}
