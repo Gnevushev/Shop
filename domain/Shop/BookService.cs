39f09cfd28446072952aabb3d303935f60f350c5
s.gnevushev <s.gnevushev@gmail.com>
2020-05-06 14:14:50 +0300

    create service for call method by query

---
+++ domain/Shop/BookService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop
+{
+   public class BookService
+    {
+
+        private readonly IBookRepository bookRepository;
+
+        public BookService(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public List<Book> GetAllByQuery(string query)
+        {
+            if (Book.IsIsbn(query))
+            {
+                return bookRepository.GetAllByISBN(query);
+            }
+            else return bookRepository.GetAllByTitleOrAuthor(query);
+
+        }
+    }
+}
